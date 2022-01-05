936b8037b915f2b07f8284f57994a00966ace7a1
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2022-01-05 10:11:17 +0100

    Add isHoliday support for Mondays/Fridays next to other holidays (bridge weekends)

--- is_holiday.go
+++ is_holiday.go
@@ -7,7 +7,7 @@ import (
 	"github.com/rickar/cal/v2/se"
 )
 
-func IsHoliday(t time.Time) bool {
+func setup() *cal.BusinessCalendar {
 	c := cal.NewBusinessCalendar()
 	h := append(se.Holidays, &cal.Holiday{
 		Name:   "Kristi himmelsfärdsdag (fredag)",
@@ -16,6 +16,24 @@ func IsHoliday(t time.Time) bool {
 	})
 
 	c.AddHoliday(h...)
+
+	return c
+}
+
+func IsHoliday(t time.Time) bool {
+	tomorrow := t.AddDate(0, 0, 1)
+	yesterday := t.AddDate(0, 0, -1)
+	weekday := t.Weekday().String()
+
+	c := setup()
+
+	if (weekday == "Monday" && !c.IsWorkday(tomorrow)) {
+		return true
+	}
+
+	if (weekday == "Friday" && !c.IsWorkday(yesterday)) {
+		return true
+	}
 
 	return !c.IsWorkday(t)
 }
--- is_holiday_test.go
+++ is_holiday_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TestIsHoliday(t *testing.T) {
+func TestIsHolidayAscensionFriday(t *testing.T) {
 	ascensionFriday := time.Date(2021, 05, 14, 12, 00, 00, 00, time.Local)
 	result := IsHoliday(ascensionFriday)
 
@@ -18,5 +18,35 @@ func TestIsHoliday(t *testing.T) {
 
 	if result != false {
 		t.Fatalf("Expected non holiday to be 'false', but got '%t'", result)
+	}
+}
+
+func TestIsHolidayMonday(t *testing.T) {
+	mondayBeforeRegularTuesday := time.Date(2021, 02, 01, 12, 00, 00, 00, time.Local)
+	result := IsHoliday(mondayBeforeRegularTuesday)
+
+	if result != false {
+		t.Fatalf("Expected Monday before regular Tuesday not to be a holiday, but got '%t'", result)
+	}
+
+	mondayBeforeHolidayTuesday := time.Date(2023, 06, 06, 12, 00, 00, 00, time.Local)
+	result = IsHoliday(mondayBeforeHolidayTuesday)
+	if result == false {
+		t.Fatalf("Expected Monday before holiday Tuesday to be a holiday, but got '%t'", result)
+	}
+}
+
+func TestIsHolidayFriday(t *testing.T) {
+	fridayAfterRegularThursday := time.Date(2022, 01, 14, 12, 00, 00, 00, time.Local)
+	result := IsHoliday(fridayAfterRegularThursday)
+
+	if result != false {
+		t.Fatalf("Expected Friday after regular Thursday not to be a holiday, but got '%t'", result)
+	}
+
+	fridayAfterHolidayThursday := time.Date(2022, 01, 07, 12, 00, 00, 00, time.Local)
+	result = IsHoliday(fridayAfterHolidayThursday)
+	if result == false {
+		t.Fatalf("Expected Friday after holiday Thursday to be a holiday, but got '%t'", result)
 	}
 }
