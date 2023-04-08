d483e2730b91e489630b2adaebe6058160c40169
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2023-04-08 11:19:32 +0200

    Make GetDynamodbData generic and return its result

--- get_dynamodb_data.go
+++ get_dynamodb_data.go
@@ -8,15 +8,18 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
-func GetDynamodbData(ctx context.Context, cfg aws.Config, tableName string, out interface{}) error {
+func GetDynamodbData[T any](ctx context.Context, cfg aws.Config, tableName string) ([]T, error) {
 	svc := dynamodb.NewFromConfig(cfg)
 	result, err := svc.Scan(ctx, &dynamodb.ScanInput{
 		TableName: aws.String(tableName),
 	})
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return attributevalue.UnmarshalListOfMaps(result.Items, out)
+	items := make([]T, result.Count)
+	err = attributevalue.UnmarshalListOfMaps(result.Items, items)
+
+	return items, err
 }
